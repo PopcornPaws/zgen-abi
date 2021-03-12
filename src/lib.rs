720e4d2f0e7bf0c1fec159e56c1efb5d5109d7bf
Mark Melczer <melczer7@gmail.com>
2021-03-12 08:57:29 +0100

    A bit of code restructuring.

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,12 @@
 #![allow(unused)]
+mod types;
+pub use types::EthereumTypes;
+
 use sha3::{Digest, Keccak256};
 
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-
-pub enum EthereumTypes {
-    /// U160 - unsigned 160 bit number
-    Address([u8; 20]),
-    /// U256 - unsigned 256 bit number
-    U256([u8; 32]),
-}
-
-impl EthereumTypes {
-    #[inline]
-    fn name_as_str(&self) -> &str {
-        match self {
-            Self::Address(_) => "address",
-            Self::U256(_) => "uint256",
-        }
-    }
-
-    #[inline]
-    fn value_as_u256(&self) -> [u8; 32] {
-        match self {
-            Self::Address(val) => {
-                let mut extended = [0_u8; 32];
-                // extend the 20 byte address by writing it to a 32 byte zero array
-                for i in 12..32 {
-                    extended[i] = val[i - 12];
-                }
-                extended
-            }
-            Self::U256(val) => *val,
-        }
-    }
-
-    #[inline]
-    fn address_from_bytes(bytes: &[u8]) -> Self {
-        assert!(bytes.len() <= 20, "Byte array doesn't fit into 160 bits");
-        let mut val = [0_u8; 20];
-        let diff = 20 - bytes.len();
-        for i in diff..20 {
-            val[i] = bytes[i - diff];
-        }
-        Self::Address(val)
-    }
-
-    #[inline]
-    fn u256_from_bytes(bytes: &[u8]) -> Self {
-        assert!(bytes.len() <= 32, "Byte array doesn't fit into 160 bits");
-        let mut val = [0_u8; 32];
-        let diff = 32 - bytes.len();
-        for i in diff..32 {
-            val[i] = bytes[i - diff];
-        }
-        Self::U256(val)
-    }
-}
 
 fn transaction(
     path_to_abi: &Path,
